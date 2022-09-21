b2cbc19f991e87a49c1929901589f30b8cfeba3c
Colleen Garvey <colleenellengarvey@gmail.com>
2022-09-21 15:11:56 -0400

    create components

---
+++ src/components/Navbar.js
@@ -0,0 +1,63 @@
+import { Link } from "react-scroll";
+import { SocialIcon } from "react-social-icons"
+
+export default function Navbar() {
+  return (
+    <div className="App">
+      <header className="nav">
+        <nav className="nav__container__actions">
+          <ul>
+            <li>
+              <Link smooth spy to="home">
+                HOME
+              </Link>
+            </li>
+            <li>
+              <Link smooth spy to="projects">
+                PROJECTS
+              </Link>
+            </li>
+            <li>
+              <Link smooth spy to="about">
+                ABOUT
+              </Link>
+            </li>
+            <li>
+              <Link smooth spy to="contact">
+                CONTACT
+              </Link>
+            </li>
+          </ul>
+
+          <div className="web-links">
+            <SocialIcon
+              url="https://github.com/cogarvey"
+              target="_blank"
+              bgColor="black"
+              fgColor="#f2ebe9"
+              style={{
+                height: "2.4vw",
+                width: "2.4vw",
+                maxHeight: "40px",
+                maxWidth: "40px",
+              }}
+            />
+            <SocialIcon
+              url="https://www.linkedin.com/in/cogarvey"
+              target="_blank"
+              bgColor="black"
+              fgColor="#f2ebe9"
+              style={{
+                height: "2.4vw",
+                maxHeight: "40px",
+                maxWidth: "40px",
+                width: "2.4vw",
+                marginLeft: "1.5vw",
+              }}
+            />
+          </div>
+        </nav>
+      </header>
+    </div>
+  );
+}

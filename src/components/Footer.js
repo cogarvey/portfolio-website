8c4c157cf4c8c6316ba7cf61e8d5f53e5039ac6b
Colleen Garvey <colleenellengarvey@gmail.com>
2022-09-21 15:27:56 -0400

    context created for footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,13 @@ import React from 'react';
 
 export default function Footer() {
   return(
-
+    <div className='footer'>
+      <p>
+        This page is created by Colleen Garvey
+      </p>
+      <p>
+      © 2022
+      </p>
+    </div>
   )
 }

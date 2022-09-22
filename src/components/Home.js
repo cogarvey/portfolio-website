c400186f9a8dd7f38bea0e077ebf7ddd04d31232
Colleen Garvey <colleenellengarvey@gmail.com>
2022-09-22 19:18:54 -0400

    create name animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,13 @@ export default function Home() {
   return(
     <div>
       <header>
-      
+      <div class="container">
+  <div class="row">
+    <div class="col-md-12 text-center">
+      <h3 class="animate-charcter"> Colleen Garvey</h3>
+    </div>
+  </div>
+</div>
       </header>
     </div>
   )

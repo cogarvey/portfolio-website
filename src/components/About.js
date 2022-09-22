327c418d7613f732e6388970884306441b7fc46d
Colleen Garvey <colleenellengarvey@gmail.com>
2022-09-22 18:50:32 -0400

    add skill icons and a bit of about me discription

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,45 @@ import React from 'react';
 export default function About() {
   return(
     <div>
-      
+      <h1>About</h1>
+      <div className='about-container'>
+      <div className='skills-container'>
+      <h3>Languages / Frameworks / Libraries:</h3>
+ <p>
+    <img alt="javascript" height="48px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg" />
+    <img alt="ruby" height="48px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ruby/ruby-original-wordmark.svg" />
+    <img alt="rails" height="48px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/rails/rails-plain-wordmark.svg" /> 
+   <img alt="react" height="48px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" />
+    <img alt="html5" height="48px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg" />
+    <img alt="bootstrap" height="48px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-plain-wordmark.svg" />
+    <img alt="css3" height="48px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg" />
+    <img alt="sass" height="48px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg" />
+      <img alt="material-UI" height="45px" src="https://mui.com/static/logo.png" />
+    <img alt="chakra-UI" height="40px" src="https://www.coffeeclass.io/logos/chakra-ui.png" />
+    <img alt="jquery" height="48px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jquery/jquery-original-wordmark.svg" />
+    <img alt="nodejs" height="48px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg" />
+    <img alt="express" height="48px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original-wordmark.svg" />
+   </p>
+  <h3>Testing:</h3>
+  <p>
+    <img alt="mocha" height="48px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mocha/mocha-plain.svg" />
+    <img alt="chai" height="45px" src="https://avatars.githubusercontent.com/u/1515293?s=280&v=4" />
+    <img alt="jest" height="48px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg" />
+    <img src="https://raw.githubusercontent.com/simple-icons/simple-icons/6e46ec1fc23b60c8fd0d2f2ff46db82e16dbd75f/icons/cypress.svg" alt="cypress" width="40" height="40"/> 
+    <img alt="eslint" height="48px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/eslint/eslint-original-wordmark.svg" />
+ </p>
+ <h3>Database & Others:</h3>
+  <p>
+    <img alt="postgresql" height="48px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original-wordmark.svg" />
+    <img alt="git" height="48px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original-wordmark.svg" />
+    <img alt="storybook" height="45px" src="https://avatars.githubusercontent.com/u/22632046?s=200&v=4" />
+</p>
+      </div>
+      <div>
+        <p>I'm a Full Stack Web Developer, just recently graduated with a background in Customer Service/IT.</p>
+        <p>Currently learning: Typescript!</p>
+      </div>
+      </div>
     </div>
   )
 }

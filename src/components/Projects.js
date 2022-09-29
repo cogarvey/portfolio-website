5633f09d07993f19c7c9dd67dbf2f54a24452f66
Colleen Garvey <colleenellengarvey@gmail.com>
2022-09-29 12:50:18 -0400

    add project area for spacing

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,22 @@
-import React from 'react';
-
+import React from "react";
 
 export default function Projects() {
-  return(
+  return (
     <div>
-      
+      <h1 className="projects-title">Projects</h1>
+      <header>
+        <div class="project-container">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+      </header>
     </div>
-  )
+  );
 }
